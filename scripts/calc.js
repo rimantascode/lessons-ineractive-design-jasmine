ccda121ccab29763392c52216d038fc8eb07eb2c
rimantascode <r.preiks@gmail.com>
2020-02-23 09:57:03 +0000

    Jasmine testing examples

---
+++ scripts/calc.js
@@ -0,0 +1,46 @@
+Calculator = function() {
+    this.value = 0;
+};
+
+Calculator.prototype.add = function(number) {
+    if (typeof(number) == "number") {
+        this.value += number;
+    } else {
+        alert("Error!");
+    }
+};
+
+whatCanIDrink = function(age) {
+	if (age < 0) {
+		return "Sorry. I can’t tell what drink because that age is incorrect!";
+	}
+	else if (age < 14) {
+		return "Drink Toddy";
+	}
+	else if (age < 18 && age >=14) {
+		return "Drink Coke";
+	}
+	else if (age < 21 && age >=18) {
+		return "Drink Beer";
+	}
+	else if (age < 130) {
+		return "Drink Whisky";
+	} 
+	else {
+		return "Sorry. I can’t tell what drink because that age is incorrect!"
+	}
+};
+fizzBuzz = function(number) {
+	if (number % 3 === 0 && number % 5 === 0) {
+		return "FizzBuzz";
+	}
+	else if (number % 3 === 0) {
+		return "Fizz";
+	}
+	else if (number % 5 === 0) {
+		return "Buzz";
+	}	
+	else {
+		return number
+	}
+};
